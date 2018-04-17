fe95acdfa570f2bf542d24e33cd6154b3546a783
Zhou <540376482yzb@gmail.com>
2018-04-17 07:36:09 -0700

    set form firstName to firstname

--- src/components/registration-form.js
+++ src/components/registration-form.js
@@ -12,8 +12,8 @@ const matchesPassword = matches("password")
 
 export class RegistrationForm extends React.Component {
 	onSubmit(values) {
-		const { username, password, firstName } = values
-		const user = { username, password, firstName }
+		const { username, password, firstname } = values
+		const user = { username, password, firstname }
 		return this.props.dispatch(localRegister(user)).then(() => this.props.dispatch(login(username, password)))
 	}
 
