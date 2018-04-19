9e009035193c422af0e1a0d3b3daa0164f283aa8
Adrian <adriantoddross@gmail.com>
2018-04-19 17:37:15 -0400

    add placeholder text and format for readability

--- src/components/registration-form.js
+++ src/components/registration-form.js
@@ -24,18 +24,32 @@ export class RegistrationForm extends React.Component {
 	render() {
 		return (
 			<form className="login-form" onSubmit={this.props.handleSubmit(values => this.onSubmit(values))}>
-				<label htmlFor="firstName">First name</label>
-				<Field component={Input} type="text" name="firstname" />
-				<label htmlFor="username">Username</label>
-				<Field component={Input} type="text" name="username" validate={[required, nonEmpty, isTrimmed]} />
+				<label htmlFor="firstName">First Name</label>
+				<Field
+					component={Input}
+					type="text"
+					name="firstname"
+					placeholder="Sally"/>
+				<label htmlFor="username" >Username</label>
+				<Field component={Input}
+					type="text"
+					name="username"
+					placeholder="sally123"
+					validate={[required, nonEmpty, isTrimmed]} />
 				<label htmlFor="password">Password</label>
-				<Field component={Input} type="password" name="password" validate={[required, passwordLength, isTrimmed]} />
-				<label htmlFor="passwordConfirm">Confirm password</label>
+				<Field
+					component={Input}
+					type="password"
+					name="password"
+					placeholder="••••••••"
+					validate={[required, passwordLength, isTrimmed]} />
+				<label htmlFor="passwordConfirm" >Confirm Password</label>
 				<Field
 					component={Input}
 					type="password"
 					name="passwordConfirm"
 					validate={[required, nonEmpty, matchesPassword]}
+					placeholder="••••••••"
 				/>
 				<button type="submit" disabled={this.props.pristine || this.props.submitting}>
 					Register
