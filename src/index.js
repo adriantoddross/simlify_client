e389f3a259ef4b8e74116782b9ade8d4c9c66e95
Adrian <adriantoddross@gmail.com>
2018-04-19 14:27:50 -0400

    install material-ui

--- src/index.js
+++ src/index.js
@@ -4,13 +4,16 @@ import {BrowserRouter as Router} from 'react-router-dom';
 import {Provider} from 'react-redux';
 import App from './components/app';
 import store from './store';
+import MuiThemeProvider from "material-ui/styles/MuiThemeProvider"
 import './index.css';
 
 ReactDOM.render(
-    <Provider store={store}>
-        <Router>
-            <App />
-        </Router>
-    </Provider>,
-    document.getElementById('root')
+	<Provider store={store}>
+		<Router>
+			<MuiThemeProvider>
+				<App />
+			</MuiThemeProvider>
+		</Router>
+	</Provider>,
+	document.getElementById('root')
 );
