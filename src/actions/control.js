44bb03a48e3b838b7124e10d40c97bf5cbc7a315
Adrian <adriantoddross@gmail.com>
2018-04-19 15:44:57 -0400

    create closeDialog action

--- src/actions/control.js
+++ src/actions/control.js
@@ -12,3 +12,8 @@ export const OPEN_DIALOG = "OPEN_DIALOG"
 export const openDialog = () => ({
 	type: OPEN_DIALOG
 })
+
+export const CLOSE_DIALOG = "CLOSE_DIALOG"
+export const closeDialog = () => ({
+	type: CLOSE_DIALOG
+})
