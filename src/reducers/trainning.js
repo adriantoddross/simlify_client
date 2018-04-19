c7a5a3d1bc8ebcc5b940f70862b94b256a08bbae
Adrian <adriantoddross@gmail.com>
2018-04-18 23:48:43 -0400

    import and use submitAnswer actions

--- src/reducers/trainning.js
+++ src/reducers/trainning.js
@@ -1,4 +1,4 @@
-import { FETCH_QUESTION_REQUEST, FETCH_QUESTION_SUCCESS, FETCH_QUESTION_ERROR, RECEIVE_FEEDBACK, GENERATE_QUESTIONS_REQUEST, GENERATE_QUESTIONS_SUCCESS, GENERATE_QUESTIONS_ERROR } from "../actions/trainning"
+import { FETCH_QUESTION_REQUEST, FETCH_QUESTION_SUCCESS, FETCH_QUESTION_ERROR, RECEIVE_FEEDBACK, GENERATE_QUESTIONS_REQUEST, GENERATE_QUESTIONS_SUCCESS, GENERATE_QUESTIONS_ERROR, SUBMIT_ANSWER_REQUEST, SUBMIT_ANSWER_SUCCESS, SUBMIT_ANSWER_ERROR } from "../actions/trainning"
 
 const initialState = {
 	name: null,
@@ -27,6 +27,13 @@ export default function trainingReducer(state=initialState, action) {
 	case GENERATE_QUESTIONS_SUCCESS : return Object.assign({}, state, {loading: false});
 
 	case GENERATE_QUESTIONS_ERROR : return Object.assign({}, state, {loading: false, error: action.error});
+
+	case SUBMIT_ANSWER_REQUEST : return Object.assign({}, state, {loading: true});
+
+	case SUBMIT_ANSWER_SUCCESS : return Object.assign({}, state, {feedback: action.feedback, loading: false});
+
+	case SUBMIT_ANSWER_ERROR : return Object.assign({}, state, {loading: false, error: action.error});
+
 
 	default : return state;
 	}
