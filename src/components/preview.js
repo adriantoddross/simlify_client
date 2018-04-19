e6326856d8bdc9f93dc35fa8e6041b4c9aff9080
Adrian <adriantoddross@gmail.com>
2018-04-19 14:16:21 -0400

    add a prop for message

--- src/components/preview.js
+++ src/components/preview.js
@@ -1,13 +1,13 @@
 import React from "react";
 
-export function preview(props) {
+export default function Preview(props) {
 	return (
 		<div>
 			<header>
 			  <h1>{props.greeting}, {props.name}!</h1>
 			</header>
 			<main>
-				<h2>Your last learned word was</h2>
+				<h2>{props.message}</h2>
 				<p>{props.question}</p>
 			</main>
 		</div>
