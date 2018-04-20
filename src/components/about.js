a45fe835e5a2471b8624840105e811b22e2be5d0
Adrian <adriantoddross@gmail.com>
2018-04-20 01:41:22 -0400

    reorganize return

--- src/components/about.js
+++ src/components/about.js
@@ -2,6 +2,8 @@ import React from "react"
 import { openSignUp, openDialog } from "../actions/control"
 import { connect } from "react-redux"
 import '../css/about.css'
+import Sims from '../sims.png'
+import MediaQuery from 'react-responsive';
 
 export function About(props) {
 	const { dispatch } = props
@@ -11,17 +13,18 @@ export function About(props) {
 	}
 	return (
 		<div className="about">
-			<main role="main" className="about-container">
-				<section className="about-info">
-					<h1 className="about-header">Learn at your own pace.</h1>
-					<p className="about-desc"><b>Simlify</b> is an app that teaches Simlish using spaced repetition. Simlish is a fictional language featured in The Sims, a video-game franchise by EA Games.</p>
-					<button className="about-button" onClick={() => handleOpenDialog()}>Get started</button>
-				</section>
-				<section className="about-image">
-					<img src="https://www.actualidadsims.com/uploads/monthly_2017_04/large.ls4-render-selfie-2.png.6bc76fdee9166ef23697a2390f8bbb68.png" alt="4 Sims taking a selfie"/>
-				</section>
-			</main>
-			<aside>{/* todo: Gif for how app works */}</aside>
+			<MediaQuery query="(min-device-width: 320px)">
+				<main role="main" className="about-container">
+					<section className="about-image">
+						<img src={Sims} alt="4 Sims taking a selfie"/>
+					</section>
+					<section className="about-info">
+						<h1 className="about-header">Learn Simlish at your own pace.</h1>
+						<p><b>Simlify</b> is an app that teaches Simlish using spaced repetition. Simlish is a fictional language featured in The Sims, a video-game franchise by EA Games.</p>
+						<button className="about-button" onClick={() => handleOpenDialog()}>Get started</button>
+					</section>
+				</main>
+			</MediaQuery>
 		</div>
 	)
 }
