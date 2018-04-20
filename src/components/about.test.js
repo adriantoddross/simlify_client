f02a85e7f65ab99af88771cee92cdd597a1c29cd
Adrian <adriantoddross@gmail.com>
2018-04-20 13:32:55 -0400

    render about component

--- src/components/about.test.js
+++ src/components/about.test.js
@@ -3,7 +3,9 @@ import {shallow, mount} from 'enzyme';
 import { About } from './about';
 
 describe('<About/>', () => {
+
 	it('Renders without crashing', () => {
 		shallow(<About />);
 	});
+
 });
