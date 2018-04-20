6a1700fa742418cc8c07f2deacf593e3568fecec
Zhou <540376482yzb@gmail.com>
2018-04-19 20:18:33 -0700

    style header

--- src/components/brand.js
+++ src/components/brand.js
@@ -1,5 +1,17 @@
 import React from "react"
 
 export default function Brand(props) {
-	return <h2 className="logo">{props.title}</h2>
+	return (
+		<h3
+			style={{
+				margin: "0",
+				color: "white",
+				fontFamily: "Cabin Sketch",
+				fontWeight: "bold",
+				fontSize: "24px"
+			}}
+		>
+			{props.title}
+		</h3>
+	)
 }
--- src/components/header-bar.js
+++ src/components/header-bar.js
@@ -4,6 +4,7 @@ import { clearAuth } from "../actions/auth"
 import { clearAuthToken } from "../local-storage"
 import Brand from "./brand"
 import { openLogin, openSignUp, openDialog, closeDialog } from "../actions/control"
+import "../css/header-nav.css"
 
 export class HeaderBar extends React.Component {
 	logOut() {
@@ -27,21 +28,33 @@ export class HeaderBar extends React.Component {
 		let logOutButton
 		let unAuthorizedButton
 		if (this.props.loggedIn) {
-			logOutButton = <button className="header-primary-button" onClick={() => this.logOut()}>Log out</button>
+			logOutButton = (
+				<button className="header-primary-button" onClick={() => this.logOut()}>
+					Log out
+				</button>
+			)
 		} else {
 			unAuthorizedButton = (
 				<div>
-					<button className="header-button" onClick={() => this.signUp()}>Sign Up</button>
-					<button className="header-primary-button" onClick={() => this.logIn()}>Log In</button>
+					<button className="header-button" onClick={() => this.signUp()}>
+						Sign Up
+					</button>
+					<button className="header-primary-button" onClick={() => this.logIn()}>
+						Log In
+					</button>
 				</div>
 			)
 		}
 
 		return (
 			<div className="header-bar">
-				<Brand title="Simlify" />
-				{logOutButton}
-				{unAuthorizedButton}
+				<div className="header-brand-container">
+					<Brand title="Simlify" />
+				</div>
+				<div className="header-buttons-container">
+					{logOutButton}
+					{unAuthorizedButton}
+				</div>
 			</div>
 		)
 	}
